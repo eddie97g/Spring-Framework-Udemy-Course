33da86ba1fd4118d438afe78e693dbb69b19a832
DESKTOP-1L2LVO0\eddie <eddie97g@gmail.com>
2020-04-19 21:04:21 -0500

    end of one to many unidirectional section

---
+++ hb-04-one-to-many-uni/src/com/luv2code/hibernate/DeleteInstructorDetailDemo.java
@@ -0,0 +1,46 @@
+package com.luv2code.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory sessionFactory = new Configuration()
+										.configure()
+										.addAnnotatedClass(Instructor.class)
+										.addAnnotatedClass(InstructorDetail.class)
+										.buildSessionFactory();
+		
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {
+			
+			session.beginTransaction();
+			
+			InstructorDetail instructorDetail = session.get(InstructorDetail.class, 4);
+			if (instructorDetail != null) {
+				instructorDetail.getInstructor().setInstructorDetail(null);
+				session.delete(instructorDetail);
+			}
+			
+			session.getTransaction().commit();
+		}
+		
+		catch(Exception exception) {
+			exception.printStackTrace();
+		}
+		
+		finally {
+			session.close();
+			sessionFactory.close();
+		}
+
+	}
+
+}
